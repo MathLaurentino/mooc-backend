e671e11785f9986df6bc13fb227195488424761a
FelipeSGoncalves <felipe.goncalves.foz@hotmail.com>
2025-11-25 11:01:10 -0300

    feat: Cors

--- src/main/java/ifpr/edu/br/mooc/config/CorsConfig.java
+++ src/main/java/ifpr/edu/br/mooc/config/CorsConfig.java
@@ -7,14 +7,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://200.17.101.2:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+                        .allowedOrigins(
+                                "http://200.17.101.2:3000",
+                                "http://200.17.101.2",
+                                "http://localhost:3000"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
             }
         };
     }
